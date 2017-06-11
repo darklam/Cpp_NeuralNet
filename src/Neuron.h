f665702f3720b69a7b936f9c38bbe17889194f70
Fakez <lamisjohn@gmail.com>
2017-06-11 21:09:46 +0300

    Neuron class definition

--- src/Neuron.h
+++ src/Neuron.h
@@ -1,7 +1,19 @@
 #ifndef NEURON_H
 #define NEURON_H
 
+#include "Functions.h"
+
 class Neuron{
+private:
+  double bias, wBias; //The bias and the weight of the bias
+  int inputs; //The amount of inputs the neuron takes
+  double *weights; //The weights of the neuron for each input
+  Functions f;
+
+public:
+  Neuron(int);
+  double feedNeuron(double *);
+  void train(double, double *);
 
 };
 

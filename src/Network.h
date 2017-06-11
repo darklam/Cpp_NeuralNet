20f9e04997e3bd7a823567b8fb5955c261fe77dc
John Lamis <lamisjohn@gmail.com>
2017-06-11 18:29:18 +0300

    Removed some stupid code

--- src/Network.h
+++ src/Network.h
@@ -1,20 +1,9 @@
-#include "Neuron.h"
-#include <vector>
+#ifndef NETWORK_H
+#define NETWORK_H
 
+#include "Neuron.h"
 class Network{
 
-private:
-	int inputs, hid1, hid2;
-	std::vector<Neuron> input, h1, h2;
-	Neuron output;
-	std::vector<double> feedInputs(std::vector<double>);
-	std::vector<double> feedHid1(std::vector<double>);
-	std::vector<double> feedHid2(std::vector<double>);
-	std::vector<double> inputsOutput, hid1Output, hid2Output;
-
-public:
-	Network(int);
-	void trainNetwork(double, std::vector<double>);
-	double getNumber(std::vector<double>);
-	int getRes(std::vector<double>);
 };
+
+#endif
--- src/Neuron.h
+++ src/Neuron.h
@@ -1,19 +1,8 @@
-#include <vector>
+#ifndef NEURON_H
+#define NEURON_H
 
 class Neuron{
 
-public:
-	Neuron(int);
-	double feedNeuron(std::vector<double>);
-	int activate(std::vector<double>);
-	void train(double, std::vector<double>);
-	void networkTrain(double, std::vector<double>);
-
-private:
-	double f(double);
-	double fdiff(double);
-	double getFault(std::vector<double>);
-	int inputs;
-	std::vector<double> weights;
-	double bias, wBias, learningConstant;
 };
+
+#endif

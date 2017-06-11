0f57588e8108e7bedb4d441d15bd6cce451b249c
Fakez <lamisjohn@gmail.com>
2017-06-11 21:03:29 +0300

    Initial commit

---
+++ src/Functions.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+class Functions{  //A class for the mathematical functions used
+public:
+  double sigmoid(double);  //The sigmoid function
+
+};
+
+#endif
